6e55710efbda6575e4ac92b118fc8a699dd5e9fb
sanyamdtu <sanyamexam@gmail.com>
2020-02-09 20:54:48 +0530

    practical approach for using promise

--- javascript/promise_async/parctical_promise.js
+++ javascript/promise_async/parctical_promise.js
@@ -1,47 +1,49 @@
 function download(url) {
-    let filename = url.split(":")[0]
-    if (!filename)
-        reject(new console.error("the url does not start with http"));
-    else {
-        return new Promise(function(resolve, reject) {
-            console.log("the file is starting to download")
+    return new Promise(function(resolve, reject) {
+        let filename = url.split(":")[0]
+        console.log(filename.toString());
+        if (!filename.toString().startsWith("http"))
+            reject(new console.error("the url does not start with http"));
+        else {
+            console.log("the file is starting to download " + filename)
             setTimeout(function() {
-                console.log("the file has downloaded after 3 seconds")
+                //          console.log("the file has downloaded after 3 seconds")
+                filename = url.split("/")[1] + "/downloaded/desktop.png";
+                resolve(filename)
             }, 3000)
-            let filename = url.split("/")[1] + "/downloaded/desktop";
-            resolve(filename)
-        })
-    }
+        }
+    })
 }
 
 function resize(filename) {
-    let name = filename.split(".")[1] + "resized_at_png"
-    if (!name) {
-        reject(new console.error("the file cannot be resized as extension png"))
-    } else {
-        return new Promise(function(resolve, reject) {
-            console.log("the file is starting to be resize")
+    return new Promise(function(resolve, reject) {
+        let name = filename.split("/")[1] + "resized_at_png"
+        if (!filename.endsWith(".png")) {
+            reject(new console.error("the file cannot be resized as extension png"))
+        } else {
+            console.log("the file is starting to be resize " + name)
             setTimeout(() => {
-                console.log("the file is resized after 3 seconds")
+                resolve(name);
+
+                //    console.log("the file is resized after 3 seconds")
             }, 3000);
-            resolve(name);
-        })
-    }
+        }
+    })
 }
 
 function upload(resizedfilename) {
-    let uploadfilename = resizedfilename.split(_at)[1] + "uploaded_at_web services";
     return new Promise(function(resolve, reject) {
-        console.log("the file is starting to upload")
+        let uploadfilename = resizedfilename.split("")[1] + "uploaded_at_web services";
+        console.log("the file is starting to upload " + uploadfilename)
         setTimeout(() => {
-            console.log("the file is uploaded  to the " + uploadfilename)
+            //      console.log("the file is uploaded  to the " + uploadfilename)
+            resolve(uploadfilename);
         }, 3000);
-        resolve(uploadfilename);
     })
 }
-let start = download("http:bytenet/friends/profilepic.png")
+let start = download("http:./bytenet/friends/profilepic.png")
     .then(resize)
     .then(upload)
-    .catch(err) {
-
-    }
+    .catch(function(err) {
+        console.error(err);
+    })
