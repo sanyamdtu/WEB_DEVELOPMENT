0d51ac39842d369b79f6d5c6624625dd499484e8
sanyamdtu <sanyamexam@gmail.com>
2020-02-12 18:55:12 +0530

    file ops

---
+++ javascript/async_js/async_udemy.js
@@ -0,0 +1,15 @@
+//this code is used and implemented by seeing a udemy video of javascript
+console.log("the first thing happen")
+alert("its not going to move on")
+console.log("the second thing is called after the alert is closed")
+    //teh second segment of code is not going to run as the  js is single threaded and thats whay 
+    //we developed the asynchronous . single threaded means that the javascript is doing 
+    //one thng at a time
+    //setTimeout(() => {
+    //the javascript should onlu pay attention as it 
+    // it knows when to work        
+    //   }, timeout);
+    /* 
+    the futher reference is in notes of we development
+     */
+    //the main relative disadvantage is the nesting of callbacks
